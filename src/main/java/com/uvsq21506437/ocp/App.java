6fc3d510d00b4fc8a9188c049be2f25c38ce80e0
shadealao <alao.a.s@gmail.com>
2020-03-21 17:47:47 +0100

    ajout dans le main : je calcul la somme des salaires

--- src/main/java/com/uvsq21506437/ocp/App.java
+++ src/main/java/com/uvsq21506437/ocp/App.java
@@ -1,5 +1,8 @@
 package com.uvsq21506437.ocp;
 
+import java.util.ArrayList;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +11,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Vendeur v1 = new Vendeur(10);
+    	Vendeur v2 = new Vendeur(20);
+    	Vendeur v3 = new Vendeur(30);
+    	Employe e1 = new Employe(2005);
+    	Employe e2 = new Employe(2010);
+    	
+    	ArrayList<allEmploye>listesalarie = new ArrayList<allEmploye>(); 
+    	listesalarie.add(v1);
+    	listesalarie.add(v2);
+    	listesalarie.add(v3);
+    	listesalarie.add(e1);
+    	listesalarie.add(e2);
+    	double salaireTotal = 0;
+    	
+    	for(int i = 0; i< listesalarie.size(); i++) {
+    		salaireTotal += listesalarie.get(i).salaire();
+    	}
+    	
+    	System.out.println("Salaires total "+salaireTotal);
     }
 }
