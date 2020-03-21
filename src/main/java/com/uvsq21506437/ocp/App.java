a400e7bd998d8b7d639018a53b01de2877a5a6c0
Utilisateur <user@debian.arcahe.ovh>
2020-03-21 16:55:43 +0100

    projet initial mvn

---
+++ src/main/java/com/uvsq21506437/ocp/App.java
@@ -0,0 +1,13 @@
+package com.uvsq21506437.ocp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
