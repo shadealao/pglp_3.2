c0de3b6e0c09f79bc3b4ff86538d7a8d6daabc76
Utilisateur <user@debian.arcahe.ovh>
2020-03-21 17:19:47 +0100

    ajout class allEmploye regroupant les deux types d emplyer (classe mere)

---
+++ src/main/java/com/uvsq21506437/ocp/allEmploye.java
@@ -0,0 +1,11 @@
+package com.uvsq21506437.ocp;
+
+
+public abstract class allEmploye {
+	public abstract double salaire();
+
+}
+
+
+
+//respecte ocp car si on ajoute une fonctionalité on va étendre rajouter une classe et non en modifier une 
